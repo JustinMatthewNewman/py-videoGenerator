eab7de7562e75df19022e49d4f37ebdf90fc36e4
Justin Newman <newma4jm@dukes.jmu.edu>
2023-09-18 11:44:24 -0400

    Random background image selection.

--- videoGenerator.py
+++ videoGenerator.py
@@ -8,6 +8,13 @@ from moviepy.video.fx import fadein, fadeout
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
+from PIL import ImageFilter
+from PIL import ImageEnhance
+
+
+import random
+from os import listdir
+from os.path import isfile, join
 
 # Function to create a text clip 
 def create_text_clip(content, duration, fontsize=50, color='white'):
@@ -19,11 +26,19 @@ def create_text_clip(content, duration, fontsize=50, color='white'):
     text_clip = ImageClip(img, duration=duration)
     return text_clip
 
-def create_text_clip2(content, duration, fontsize=50, color='white'):
+def create_text_clip2(content, duration, fontsize=50, color='white', words_per_line=4):
     img = Image.new('RGBA', (854, 480), (255, 255, 255, 0))
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype('sans.ttf',50)
-    draw.text((200, 200), content, fill=color, font=font)
+    # Split the content into lines with 4 words per line
+    words = content.split()
+    lines = [' '.join(words[i:i+words_per_line]) for i in range(0, len(words), words_per_line)]
+        
+    y_position = 200
+    for line in lines:
+        draw.text((200, y_position), line, fill=color, font=font)
+        y_position += 50  # Adjust this value to control the spacing between lines
+
     img = np.array(img) 
     text_clip = ImageClip(img, duration=duration)
     return text_clip
@@ -78,12 +93,17 @@ def create_composite_video(image_path, row):
       right = left + width
       bottom = top + height
       img = img.crop((left, top, right, bottom))
-
+      enhancer = ImageEnhance.Brightness(img)
+      # to reduce brightness by 50%, use factor 0.5
+      img = enhancer.enhance(0.5)
+      img = img.filter(ImageFilter.GaussianBlur(5))
       # Save cropped image
       img.save("centered_image.jpg")
     # Load cropped image as clip
     centered_clip = ImageClip("centered_image.jpg")
     centered_clip = centered_clip.set_position("center")
+
+
 
 
     title_clip = create_text_clip(row['Title'], 7)
@@ -99,11 +119,15 @@ def create_composite_video(image_path, row):
     return final_video
 
 if __name__ == "__main__":
-    image_path = "input.jpg" 
+
+
     csv_path = "input.csv"
     df = pd.read_csv(csv_path)
 
     for index, row in df.iterrows():
+        input_images_dir = 'input_images'
+        input_images = [f for f in listdir(input_images_dir) if isfile(join(input_images_dir, f))]
+        image_path = join(input_images_dir, random.choice(input_images))
         output_video = create_composite_video(image_path, row)
         output_path = f"./render/output_{index}.mp4"
         output_video.write_videofile(output_path, codec="libx264", fps=24)
